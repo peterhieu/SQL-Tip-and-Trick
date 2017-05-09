215fe0305aa23305a33df860fc08e04c115f589a
Mai Trung Hieu <hieumt@necvietnam.hn>
2017-05-09 15:18:10 +0700

    procedure reads xml file into an xml object

--- ReadingXMLfile.sql
+++ ReadingXMLfile.sql
@@ -3,6 +3,6 @@ CRATE PROC [dbo].[GetXML]
     @xml XML output
 AS
 BEGIN
-    DECLARE @sql nVARCHAR(max) = 'SET @xml = CAST((SELECT CAST(c1 as xml) from OPENROWSET (BULK ''' + @FilePath + ''',SINGLE_BLOB) as T1(c1)) AS XML)';
+    DECLARE @sql nVARCHAR(max) = 'SET @xml = CAST((SELECT CAST(c1 AS xml) FROM OPENROWSET (BULK ''' + @FilePath + ''',SINGLE_BLOB) AS T1(c1)) AS XML)';
     EXEC sp_executesql @sql, N'@xml xml out', @xml out
 END
